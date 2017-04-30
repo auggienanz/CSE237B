a9c8b700918fddca2567c381ed0079bd73241f42
Auggie Nanz <auggienanz@gmail.com>
2017-04-30 18:48:11 -0500

    Added logging to csv and matlab plot script

---
+++ HW1/client/plotResults.m
@@ -0,0 +1,16 @@
+% CSE 237B, Lab 1, Part 1
+% August Nanz
+
+% Import data
+data = csvread('result.csv');
+rtt = data(:,1)./1e3;
+offset = data(:,2)./1e3;
+
+% Plot RTT, offset, and bound on error
+figure;
+plot(offset);
+hold on
+plot(offset - rtt./2,':');
+plot(offset + rtt./2,':');
+legend('Offset','Lower Bound','Upper Bound');
+ylabel('ms');
